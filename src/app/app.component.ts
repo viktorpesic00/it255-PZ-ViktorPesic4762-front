ba766656ea5852e014d9b856300fe1f067911c36
Viktor Pesic <viktorpesic00@gmail.com>
2023-01-11 18:35:56 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'it255-PZ-ViktorPesic4762-front';
+}
